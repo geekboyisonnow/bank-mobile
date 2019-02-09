79d69204c5bd48d89fc0b99d9462a57aa69dc99b
Kenneth Cannon <kennethfcannon@gmail.com>
2019-02-08 22:03:21 -0500

    Styling Fonts

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header>
-          Bank Mobile
+          <span className="bank">Bank</span><span>&#8239;</span><strong>OZK</strong>
         </header>
+        <body>
+          <main className="main-style">
+          <div className="main-font">
+          Hey BANK OZK Labs... Here is where all the wonderful Text is going to go and go and go and go.
+          </div>
+          </main>
+        </body>
       </div>
     );
   }
