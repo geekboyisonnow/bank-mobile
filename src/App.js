7f788a192a9ea67b1daa9cddf34ae86c7e205d14
Kenneth Cannon <kennethfcannon@gmail.com>
2019-02-10 00:13:09 -0500

    Starting Auth) Integreation Over - Updating Script - Index.html

--- src/App.js
+++ src/App.js
@@ -8,76 +8,76 @@ import './App.css';
 import Auth from './Auth/Auth.js';
 import Home from './Home.js';
 
-// const auth = new Auth();
-// auth.login();
+const auth = new Auth();
+auth.login();
 class App extends Component {
-//   goTo(route) {
-//     this.props.history.replace(`/${route}`)
-//   }
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
 
-//   login() {
-//     this.props.auth.login();
-//   }
+  login() {
+    this.props.auth.login();
+  }
 
-//   logout() {
-//     this.props.auth.logout();
-//   }
+  logout() {
+    this.props.auth.logout();
+  }
 
-//   componentDidMount() {
-//     const { renewSession } = this.props.auth;
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
 
-//     if (localStorage.getItem('isLoggedIn') === 'true') {
-//       renewSession();
-//     }
-//   }
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
 
-//   render() {
-//     const { isAuthenticated } = this.props.auth;
+  render() {
+    const { isAuthenticated } = this.props.auth;
 
-//     return (
-//       <Router>
-//       <div>
-//         <Navbar fluid>
-//           <Navbar.Header>
-//             <Navbar.Brand>
-//               <a href="#">Auth0 - React</a>
-//             </Navbar.Brand>
-//             <Button
-//               bsStyle="primary"
-//               className="btn-margin"
-//               onClick={this.goTo.bind(this, 'home')}
-//             >
-//               Home
-//             </Button>
-//             {
-//               !isAuthenticated() && (
-//                   <Button
-//                     bsStyle="primary"
-//                     className="btn-margin"
-//                     onClick={this.login.bind(this)}
-//                   >
-//                     Log In
-//                   </Button>
-//                 )
-//             }
-//             {
-//               isAuthenticated() && (
-//                   <Button
-//                     bsStyle="primary"
-//                     className="btn-margin"
-//                     onClick={this.logout.bind(this)}
-//                   >
-//                     Log Out
-//                   </Button>
-//                 )
-//             }
-//           </Navbar.Header>
-//         </Navbar>
-//       </div>
-//       {/* <Link to='Home.js'/> */}
-//       </Router>
-//     );
-//   }
-// }
+    return (
+      <Router>
+      <div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+          </Navbar.Header>
+        </Navbar>
+      </div>
+      {/* <Link to='Home.js'/> */}
+      </Router>
+    );
+  }
+}
 
 export default App;
